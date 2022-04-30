c9e8568ba5017d794703a14a47187af9908b1b21
Ryan Umstead <rjumstead@gmail.com>
2022-04-30 18:57:43 -0400

    feat: scaffolding

--- pkg/scalers/static/static.go
+++ pkg/scalers/static/static.go
@@ -30,15 +30,19 @@ func (s *Scaler) IsActive(_ context.Context, _ *pb.ScaledObjectRef) (*pb.IsActiv
 	}, nil
 }
 
-func (s *Scaler) StreamIsActive(_ *pb.ScaledObjectRef, epsServer pb.ExternalScaler_StreamIsActiveServer) error {
+func (s *Scaler) StreamIsActive(scaledObj *pb.ScaledObjectRef, epsServer pb.ExternalScaler_StreamIsActiveServer) error {
 	for {
 		select {
 		case <-epsServer.Context().Done():
 			// call cancelled
 			return nil
-		case <-time.Tick(time.Hour * 1):
-			err := epsServer.Send(&pb.IsActiveResponse{
-				Result: s.Down(),
+		case <-time.Tick(5 * time.Millisecond):
+			active, err := s.IsActive(context.TODO(), scaledObj)
+			if err != nil {
+				return err
+			}
+			err = epsServer.Send(&pb.IsActiveResponse{
+				Result: active.Result,
 			})
 			return err
 		}
